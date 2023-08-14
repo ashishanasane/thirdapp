aea5e675b65447c5b36ad085f7bd6f1ccc989089
ashishanasane <anasaneashish@gmail.com>
2023-08-14 17:56:03 +0530

    React Event Object - 'b' represents the react event that triggered the function, in this case the 'click' event

--- src/Try.js
+++ src/Try.js
@@ -5,11 +5,16 @@ const Try = () => {
     const shoot = (a) =>{
         alert(a);
     }
+    const shootevent = (a,b) =>{
+        alert(b.type);
+    }
     return(
         <>
         <button onClick={greatShoot}>Take the shot!</button>
         <br></br>
         <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+        <br></br>
+        <button onClick={(event) => shootevent("Goal",event)}>Take the shot!</button>
         </>
     );
 }
